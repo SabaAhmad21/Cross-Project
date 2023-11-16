f23a0b4ce8573a79d9e05a6b77ef04ad0c0f4c46
Saba Ahmad <sabaahmad821@gmail.com>
2023-11-16 23:36:09 +0500

    Salam

---
+++ MyProject/MyProject/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProject.ViewModels;
+
+namespace MyProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ContactCreate()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyProject/MyProject/ViewModels/Contact.cs
@@ -0,0 +1,16 @@
+﻿namespace MyProject.ViewModels
+{
+    public class Contact
+    {
+        public string FirstName {  get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string SubjectOfMessage{ get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
